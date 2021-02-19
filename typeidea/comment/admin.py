70f41374309e74a0c3350ce1842bacd5b1e17089
Salmon-Mean <loneworlf@outlook.com>
2021-02-19 10:43:54 +0800

    chapter07 finished

--- typeidea/comment/admin.py
+++ typeidea/comment/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 from typeidea.base_admin import BaseOwnerAdmin
+from typeidea.custom_site import custom_site
 from .models import Comment
 
-@admin.register(Comment)
+@admin.register(Comment, site=custom_site)
 class CommentAdmin(BaseOwnerAdmin):
     list_display = ('target', 'nickname', 'content', 'website', 'created_time')
 
--- typeidea/config/admin.py
+++ typeidea/config/admin.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
 
 from typeidea.base_admin import BaseOwnerAdmin
+from typeidea.custom_site import custom_site
 from .models import Link, SideBar
 
-@admin.register(Link)
+@admin.register(Link, site=custom_site)
 class LinkAdmin(BaseOwnerAdmin):
     list_display = ('title', 'href', 'status', 'weight', 'created_time')
     fields = ('title', 'href', 'status', 'weight')
 
 
-@admin.register(SideBar)
-class SideBarAdmin(admin.ModelAdmin):
+@admin.register(SideBar, site=custom_site)
+class SideBarAdmin(BaseOwnerAdmin):
     list_display = ('title', 'display_type', 'content', 'created_time')
     fields = ('title', 'display_type', 'content')
 
